4cc3622a0325824cd71023dade02e603fd6570f4
eeswari-s <eeswarivaithee@gmail.com>
2025-12-10 13:43:45 +0530

    c

--- Day7/index.js
+++ Day7/index.js
@@ -31,3 +31,31 @@ while (b<=10);
 //explore
 //for..of ,for...in
 
+
+//foreach
+
+const products = [
+    {id:1,name:"laptop",price:50000,inStock:true},
+    {id:2,name:"headphone",price:1500,inStock:true},
+    {id:3,name:"keyboard",price:800,inStock:false},
+    {id:4,name:"mouse",price:500,inStock:true}
+];
+
+function showProducts() {
+    products.forEach(item =>{
+        console.log("------------------");
+        console.log("product ID: ",item.id);
+        console.log("product Name: ",item.name);
+        console.log("product Price: ",item.price);
+
+        if(item.inStock)
+        {
+            console.log("status: Available");
+        }
+        else{
+            console.log("out of stock");
+        }
+     
+    });
+}
+showProducts();
