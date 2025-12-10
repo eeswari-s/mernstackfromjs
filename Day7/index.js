566f9c2c05a7354ec0a0531a1435bc6f043ca7b5
eeswari-s <eeswarivaithee@gmail.com>
2025-12-10 13:32:36 +0530

    c

---
+++ Day7/index.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+for(let i=1; i <= 10; i++)
+{
+    console.log("numbers",i);
+}
+
+
+var a=5;
+while(a <=10)
+{
+    console.log("numbers",a);
+    a++;
+}
+
+
+
+ let b =2
+do{
+    console.log("numbers",b);
+b++;
+
+}
+while (b<=10);
+
+//explore
+//for..of ,for...in
+
